0f63f1bc945fb647118ff0840efac93b20c49007
Your Name <sergioserra.2003@gmail.com>
2023-10-24 17:28:10 +0100

    Processamento Assíncrono de Imagens

--- client.py
+++ client.py
@@ -6,24 +6,45 @@ import io
 # Conecta ao servidor RPC
 server = xmlrpc.client.ServerProxy('http://server:8000')
 
-def apply_image_operation_and_save(encoded_image, operation_func, *args, save_path):
-    binary_result_image = operation_func(encoded_image, *args).data
-    decoded_result_bytes = base64.b64decode(binary_result_image)
-    result_image = Image.open(io.BytesIO(decoded_result_bytes))
-    result_image.save(save_path, 'JPEG')
+def convert_to_grayscale(encoded_image):
+    return send_task(encoded_image, server.convert_to_grayscale)
 
-def convert_to_grayscale(encoded_image, save_path):
-    apply_image_operation_and_save(encoded_image, server.convert_to_grayscale, save_path=save_path)
+def resize_image(encoded_image, width, height):
+    return send_task(encoded_image, server.resize_image, width, height)
 
-def resize_image(encoded_image, width, height, save_path):
-    apply_image_operation_and_save(encoded_image, server.resize_image, width, height, save_path=save_path)
+def rotate_image(encoded_image, angle):
+    return send_task(encoded_image, server.rotate_image, angle)
 
-def rotate_image(encoded_image, angle, save_path):
-    apply_image_operation_and_save(encoded_image, server.rotate_image, angle, save_path=save_path)
+def send_task(encoded_image, operation_func, *args):
+    return operation_func(encoded_image, *args)
+
+def save_image(image, save_path):
+    image.save(save_path, 'JPEG')
+
+def get_image(task_id):
+   binary_result_image = server.get_image(task_id).data
+   decoded_result_bytes = base64.b64decode(binary_result_image)
+   result_image = Image.open(io.BytesIO(decoded_result_bytes))
+   return result_image
+
+def get_task_status(task_id):
+    return server.get_task_status(task_id)
 
 with open("./image.jpg", 'rb') as f:
     encoded_image = base64.b64encode(f.read()).decode()
 
-convert_to_grayscale(encoded_image, '/data/grayscale_image.jpg')
-resize_image(encoded_image, 800, 600, '/data/resized_image.jpg')
-rotate_image(encoded_image, 90, '/data/rotated_image.jpg')
+task_gray = convert_to_grayscale(encoded_image)
+task_resize = resize_image(encoded_image, 800, 600)
+task_rotate = rotate_image(encoded_image, 90)
+
+
+while True:
+    if(
+        get_task_status(task_gray)['status'] == 'completed' and
+        get_task_status(task_resize)['status'] == 'completed' and
+        get_task_status(task_rotate)['status'] == 'completed'
+       ):
+            save_image(get_image(task_gray), '/data/grayscale_image.jpg')
+            save_image(get_image(task_resize), '/data/resized_image.jpg')
+            save_image(get_image(task_rotate), '/data/rotated_image.jpg')
+            break
--- server.py
+++ server.py
@@ -2,15 +2,27 @@ import base64
 from PIL import Image
 from io import BytesIO
 from xmlrpc.server import SimpleXMLRPCServer
+import threading
 
-def apply_image_operation(encoded_image, operation_func, *args):
+TASKS = {}
+
+def create_image_processing_task(encoded_image, operation, *args):
+    task_id = len(TASKS) + 1
+    TASKS[task_id] = {'status': 'processing'}
+    threading.Thread(target=process_image, args=(task_id, encoded_image, operation, *args)).start()
+    return task_id
+
+def process_image(task_id, encoded_image, operation_func, *args):
     decoded_image = base64.b64decode(encoded_image)
     with Image.open(BytesIO(decoded_image)) as image:
         result_image = operation_func(image, *args)
 
     with BytesIO() as buffered:
         result_image.save(buffered, format="JPEG")
-        return base64.b64encode(buffered.getvalue())
+        encoded_image = base64.b64encode(buffered.getvalue())
+
+    TASKS[task_id]['status'] = 'completed'
+    TASKS[task_id]['result'] = encoded_image
 
 def convert_to_grayscale(image):
     return image.convert("L")
@@ -21,11 +33,23 @@ def resize_image(image, width, height):
 def rotate_image(image, angle):
     return image.rotate(angle, expand=True)
 
-server = SimpleXMLRPCServer(('server', 8000))
+def get_task_status(task_id):
+    task_info = TASKS.get(task_id)
+    if task_info is not None:
+        return {'status': task_info.get('status')}
+    else:
+        return {'status': 'error', 'message': 'Tarefa não encontrada'}
+    
+def get_image(task_id):
+    if TASKS.get(task_id).get('status') == 'completed': return TASKS.get(task_id).get('result')
 
-server.register_function(lambda img: apply_image_operation(img, convert_to_grayscale), 'convert_to_grayscale')
-server.register_function(lambda img, w, h: apply_image_operation(img, resize_image, w, h), 'resize_image')
-server.register_function(lambda img, a: apply_image_operation(img, rotate_image, a), 'rotate_image')
+server = SimpleXMLRPCServer(('0.0.0.0', 8000))
+
+server.register_function(lambda img: create_image_processing_task(img, convert_to_grayscale), 'convert_to_grayscale')
+server.register_function(lambda img, w, h: create_image_processing_task(img, resize_image, w, h), 'resize_image')
+server.register_function(lambda img, a: create_image_processing_task(img, rotate_image, a), 'rotate_image')
+server.register_function(lambda task_id: get_task_status(task_id), 'get_task_status')
+server.register_function(lambda task_id: get_image(task_id), 'get_image')
 
 print("Servidor RPC iniciado na porta 8000...")
 server.serve_forever()
